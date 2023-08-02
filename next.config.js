587621a35b676bec6e9ba1868c0aa275e0ba7b8c
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-08-02 15:54:10 -0400

    feat: ini all styles

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['ipfs.io', 'arweave.net'],
+  },
+  reactStrictMode: false,
+  swcMinify: false,
+  webpack5: true,
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.resolve.fallback = { fs: false, net: false, tls: false };
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
