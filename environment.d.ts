1927c1bb4af927fbb3a5a88ee6274db0968fdda2
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-08-02 16:17:29 -0400

    feat: ini root layout

--- environment.d.ts
+++ environment.d.ts
@@ -1,19 +1,19 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      /* Site config */
-      PRELAUNCH: boolean;
+      // Site config
       NEXT_PUBLIC_BLOCK_EXPLORER: string;
       NEXT_PUBLIC_CHAIN_ID: 1 | 5 | 17;
-      /* Chain config */
+      // Chain config
       NEXT_PUBLIC_CURTA_ADDRESS: `0x${string}`;
       NEXT_PUBLIC_AUTHORSHIP_TOKEN_ADDRESS: `0x${string}`;
-      /* Supabase */
+      // Supabase
       NEXT_PUBLIC_SUPABASE_URL: string;
       NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
       NEXT_PUBLIC_SUPABASE_SCHEMA: `${'mainnet' | 'goerli' | 'constellation'}_${'test' | 'prod'}`;
-      /* Services */
+      // Services
       NEXT_PUBLIC_ALCHEMY_ID: string;
+      NEXT_PUBLIC_WALLETCONNECT_ID: string;
     }
   }
 }
