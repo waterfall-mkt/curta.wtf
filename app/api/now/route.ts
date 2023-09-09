ffa87a5bdd0ab4499b4cb4e5c787c1fe729f24f1
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-09-09 15:40:29 -0400

    fix: remove caching from /api/now

--- app/api/now/route.ts
+++ app/api/now/route.ts
@@ -1,6 +1,12 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function GET(request: NextRequest) {
+export async function GET() {
   return NextResponse.json({ now: Date.now() }, { status: 200 });
 }
+
+// -----------------------------------------------------------------------------
+// Next.js config
+// -----------------------------------------------------------------------------
+
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'default-no-store';
