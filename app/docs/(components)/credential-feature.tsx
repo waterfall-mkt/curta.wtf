8d2bbb0871641d2e082955ff43c4cbaa39147aaf
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-09-14 18:51:25 -0400

    fix: styles

--- app/docs/(components)/credential-feature.tsx
+++ app/docs/(components)/credential-feature.tsx
@@ -24,7 +24,7 @@ const CredentialFeature: FC<CredentialFeatureProps> = ({ href, name, description
       className="group overflow-hidden rounded-xl border border-stroke bg-gray-600 no-underline transition-colors hover:border-gray-250"
       href={href}
     >
-      <div className="flex items-center justify-center border-b border-stroke bg-gray-700 py-4 transition-colors">
+      <div className="flex items-center justify-center border-b border-stroke bg-gray-700 p-4 transition-colors">
         {children}
       </div>
       <div className="p-4">
