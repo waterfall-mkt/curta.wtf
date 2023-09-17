6163f5b8b6038034293848250ee885645adf1e78
5/9 <66016924+fiveoutofnine@users.noreply.github.com>
2023-09-17 11:28:17 -0400

    refactor: move logo icons to `<LogoIcon />` and `<Twitter />` -> `<LogoIcon.X />` (#21)
    * refactor: move logo icons to a common component
    
    * feat: add `XIcon`
    
    * refactor: `<Twitter />` -> `<LogoIcon.X />`
    
    * chore: remove waterfall twitter from socials

---
+++ components/common/logo-icon/index.tsx
@@ -0,0 +1,11 @@
+import { EthereumIcon, OpenSeaIcon, XIcon } from './icons';
+
+const LogoIcon = () => null;
+
+LogoIcon.Ethereum = EthereumIcon;
+LogoIcon.OpenSea = OpenSeaIcon;
+LogoIcon.X = XIcon;
+
+LogoIcon.displayName = 'LogoIcon';
+
+export default LogoIcon;
