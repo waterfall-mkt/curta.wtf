389f96fc0f15cbb0f698402af86c6d46c8ad4f94
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-09-17 10:58:26 -0400

    refactor: move logo icons to a common component

---
+++ components/common/logo-icon/index.tsx
@@ -0,0 +1,10 @@
+import { EthereumIcon, OpenSeaIcon } from './icons';
+
+const LogoIcon = () => null;
+
+LogoIcon.Ethereum = EthereumIcon;
+LogoIcon.OpenSea = OpenSeaIcon;
+
+LogoIcon.displayName = 'LogoIcon';
+
+export default LogoIcon;
