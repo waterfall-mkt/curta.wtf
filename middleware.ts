58dee1093b71ec9908de90e0d45e1c01ac9a2e67
5/9 <66016924+fiveoutofnine@users.noreply.github.com>
2023-11-06 10:43:03 -0500

    feat: add redirects (#28)

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,17 @@ import { type NextRequest, NextResponse } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/docs/intro') {
+  if (request.nextUrl.pathname === '/docs/overview') {
     return NextResponse.redirect(new URL('/docs', request.url));
-  }
-
-  if (request.nextUrl.pathname === '/guides') {
-    return NextResponse.redirect(new URL('/guides/player/getting-started', request.url));
+  } else if (request.nextUrl.pathname === '/docs/puzzles') {
+    return NextResponse.redirect(new URL('/docs/puzzles/overview', request.url));
+  } else if (request.nextUrl.pathname === '/attend') {
+    // Temporary redirect to Curta Cup form
+    return NextResponse.redirect(new URL('https://forms.gle/kYLwBqWSFc59efoc7', request.url));
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/docs/intro', '/guides'],
+  matcher: ['/docs/overview', '/docs/puzzles', '/attend'],
 };
