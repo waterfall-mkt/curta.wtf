a276ac999382dae36330d71c89a49bfe3cb555fd
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-11-09 12:54:51 -0500

    feat: add middleware to rewrite puzzle pages

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,25 @@
 import { type NextRequest, NextResponse } from 'next/server';
 
+import { getChainIdAndId } from './lib/utils';
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === '/docs/intro') {
     return NextResponse.redirect(new URL('/docs', request.url));
-  }
-
-  if (request.nextUrl.pathname === '/guides') {
+  } else if (request.nextUrl.pathname === '/guides') {
     return NextResponse.redirect(new URL('/guides/player/getting-started', request.url));
+  } else if (request.nextUrl.pathname.startsWith('/puzzle')) {
+    const ids = getChainIdAndId(request.nextUrl.pathname.split('/')[1] ?? '');
+    if (!ids) return;
+
+    if (ids.chainId === 1) {
+      return NextResponse.rewrite(new URL(`/puzzle/${ids.id}`, request.url));
+    }
+    return NextResponse.rewrite(new URL(`/puzzle/${ids.chainId}:${ids.id}`, request.url));
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/docs/intro', '/guides'],
+  matcher: ['/docs/intro', '/guides', '/puzzle/:slug'],
 };
