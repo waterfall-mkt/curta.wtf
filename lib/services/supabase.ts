0a8da7237b9b6342b13ebf8b33f9f1d9f6e5430f
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-11-13 18:54:00 -0500

    feat: add supabase schema differentiation

--- lib/services/supabase.ts
+++ lib/services/supabase.ts
@@ -3,6 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const options = { db: { schema: process.env.NEXT_PUBLIC_IS_TESTNET ? 'testnet' : 'public' } };
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey, options);
 
 export default supabase;
