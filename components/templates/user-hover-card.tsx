54a314f38b7cea36447532e6e47f123b83762951
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-11-15 02:29:37 -0500

    feat: impl. rest of the hover card

--- components/templates/user-hover-card.tsx
+++ components/templates/user-hover-card.tsx
@@ -4,15 +4,15 @@ import { type FC, Fragment, type ReactNode } from 'react';
 
 import AddressLinkClient from './address-link-client';
 import ENSAvatarClient from './ens-avatar-client';
-import { ExternalLink, Github } from 'lucide-react';
+import { ArrowUpRight, ExternalLink, Github, Puzzle } from 'lucide-react';
 import useSWR from 'swr';
 import type { Address } from 'viem';
 
 import { DbUser } from '@/lib/types/api';
-import { getChainInfo } from '@/lib/utils';
+import { getChainInfo, getShortenedAddress } from '@/lib/utils';
 
 import LogoIcon from '@/components/common/logo-icon';
-import { ButtonGroup, HoverCard, IconButton } from '@/components/ui';
+import { Button, ButtonGroup, HoverCard, IconButton } from '@/components/ui';
 
 type UserHoverCardProps = {
   address: Address;
@@ -37,7 +37,7 @@ const UserHoverCard: FC<UserHoverCardProps> = ({ address, trigger }) => {
       }}
       hasArrow={false}
     >
-      {!error || !isLoading || data !== undefined ? (
+      {!error && !isLoading ? (
         <Fragment>
           <div className="flex items-center justify-between">
             <ENSAvatarClient nameOrAddress={address} size={52} />
@@ -75,13 +75,60 @@ const UserHoverCard: FC<UserHoverCardProps> = ({ address, trigger }) => {
               })}
             </ButtonGroup>
           </div>
-          <div className="flex flex-col gap-0.5">
-            <span className="font-medium text-gray-50">{data?.displayName}</span>
-            <AddressLinkClient className="text-sm font-normal text-gray-200" address={address} />
+          <div className="flex flex-col gap-1">
+            <div className="flex h-5 items-center gap-2">
+              <span className="font-medium leading-5 text-gray-50">
+                {data?.displayName ?? getShortenedAddress(address)}
+              </span>
+              {data?.isPuzzleAuthor ? (
+                <span className="flex h-5 items-center rounded bg-gray-450 px-1.5 text-xs text-gray-200">
+                  Author
+                </span>
+              ) : null}
+            </div>
+            <span className="flex h-4 gap-0.5 text-sm font-normal text-gray-200">
+              <AddressLinkClient className="leading-4" address={address} />
+              <ArrowUpRight className="h-2.5 w-2.5" />
+            </span>
           </div>
           {data?.bio ? <div className="text-sm text-gray-100">{data.bio}</div> : null}
+          <span className="flex items-center gap-1 text-sm text-gray-200">
+            <Puzzle className="h-3.5 w-3.5" />
+            <span className="font-medium text-gray-100">{data?.puzzlesSolved ?? 0}</span>
+            <span> solves</span>
+          </span>
         </Fragment>
-      ) : null}
+      ) : isLoading ? (
+        <Fragment>
+          <div className="flex items-center justify-between">
+            <div className="h-[3.25rem] w-[3.25rem] animate-pulse rounded-full bg-gray-350" />
+            <IconButton
+              intent="neutral"
+              variant="outline"
+              href={`https://${getChainInfo(1).blockExplorer}/address/${address}`}
+              newTab
+            >
+              <ExternalLink />
+            </IconButton>
+          </div>
+          <div className="flex flex-col gap-1">
+            <div className="h-5 w-[4.5rem] animate-pulse rounded-md bg-gray-350" />
+            <div className="h-4 w-[5.5rem] animate-pulse rounded-md bg-gray-350" />
+          </div>
+          <div className="flex items-center gap-2">
+            <div className="h-4 w-[5rem] animate-pulse rounded-md bg-gray-350" />
+            <div className="h-4 w-[5rem] animate-pulse rounded-md bg-gray-350" />
+          </div>
+        </Fragment>
+      ) : (
+        <div className="flex flex-col items-center">
+          <div className="text-base font-medium text-gray-50">Fetch error</div>
+          <div className="mb-2 text-sm text-gray-200">Unable to fetch user data.</div>
+          <Button className="w-fit" variant="secondary" onClick={() => mutate()}>
+            Try again
+          </Button>
+        </div>
+      )}
     </HoverCard>
   );
 };
