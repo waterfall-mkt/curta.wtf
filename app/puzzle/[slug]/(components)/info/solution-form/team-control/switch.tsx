0a126be651a8ffe51ee5620a63ecf7d3ddec50bd
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-11-21 02:18:52 -0500

    fix: () =>

--- app/puzzle/[slug]/(components)/info/solution-form/team-control/switch.tsx
+++ app/puzzle/[slug]/(components)/info/solution-form/team-control/switch.tsx
@@ -378,12 +378,14 @@ const NewTeamForm: FC<{ name: string }> = ({ name }) => {
             </div>
             <button
               className="-mx-1 flex items-center gap-0.5 rounded-sm px-1 text-xs leading-4 text-gray-200 underline decoration-dashed transition-colors hover:text-gray-100 focus:outline-none focus-visible:text-gray-100 focus-visible:ring-2 focus-visible:ring-blue-250"
-              onClick={window.open(
-                `https://${getChainInfo(chainId).blockExplorer}/address/${
-                  getChainInfo(chainId).blockExplorer
-                }`,
-                '_blank',
-              )}
+              onClick={() =>
+                window.open(
+                  `https://${getChainInfo(chainId).blockExplorer}/address/${
+                    getChainInfo(chainId).blockExplorer
+                  }`,
+                  '_blank',
+                )
+              }
             >
               <span>Invite members</span>
               <UserPlus className="h-3 w-3" />
