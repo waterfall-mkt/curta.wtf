c66b83fcb2ed39d00ee78d3922c88a0e20e10174
5/9 <66016924+fiveoutofnine@users.noreply.github.com>
2023-11-24 10:44:29 -0500

    Update redirects (#53)
    * chore: remove unused redirects
    
    * perf: move static/permanent redirects to next.config
    
    * chore: add contract redirects

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,7 @@ import { getChainIdAndId, getChainInfo } from './lib/utils';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/docs/overview') {
-    return NextResponse.redirect(new URL('/docs', request.url));
-  } else if (request.nextUrl.pathname === '/docs/puzzles') {
-    return NextResponse.redirect(new URL('/docs/puzzles/overview', request.url));
-  } else if (request.nextUrl.pathname.toLowerCase().match(/puzzle\/(?:\d+|eth):(\d+)/)) {
+  if (request.nextUrl.pathname.toLowerCase().match(/puzzle\/(?:\d+|eth):(\d+)/)) {
     const ids = getChainIdAndId(request.nextUrl.pathname.split('/')[2] ?? '');
     if (!ids) return;
 
@@ -18,21 +14,10 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(
       new URL(`/puzzle/${getChainInfo(ids.chainId).network}:${ids.id}`, request.url),
     );
-  } else if (request.nextUrl.pathname === '/attend') {
-    // Temporary redirect to Curta Cup form
-    return NextResponse.redirect(new URL('https://forms.gle/kYLwBqWSFc59efoc7', request.url));
-  } else if (request.nextUrl.pathname === '/faq') {
-    // Temporary redirect to Curta Cup FAQ
-    return NextResponse.redirect(
-      new URL(
-        'https://docs.google.com/document/d/1-SQ4es7Hpq-73OqS89ARJ6vdXdOSt6fGhtt3E4zuXC0/edit',
-        request.url,
-      ),
-    );
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/docs/overview', '/docs/puzzles', '/puzzle/:path*', '/attend', '/faq'],
+  matcher: ['/puzzle/:path*'],
 };
--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,40 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  redirects: async () => [
+    {
+      source: '/docs/overview',
+      destination: '/docs',
+      permanent: true,
+    },
+    {
+      source: '/docs/puzzles',
+      destination: '/docs/puzzles/overview',
+      permanent: true,
+    },
+    // Mainnet contract redirects
+    {
+      source: '/(1|eth)\\:(puzzles|flag)',
+      destination: 'https://etherscan.io/address/0x0000000006bC8D9e5e9d436217B88De704a9F307',
+      permanent: true,
+    },
+    {
+      source: '/(1|eth)\\:(authorship)',
+      destination: 'https://etherscan.io/address/0xC0ffeEb30F5aAA18Cd0a799F6dA1bdcb46f63C44',
+      permanent: true,
+    },
+    // Base contract redirects
+    {
+      source: '/(8453|base)\\:(puzzles|flag)',
+      destination: 'https://basescan.org/address/0x00000000d1329c5cd5386091066d49112e590969',
+      permanent: true,
+    },
+    {
+      source: '/(8453|base)\\:(authorship)',
+      destination: 'https://basescan.org/address/0xC0FFEE8b8e502403e51f37030E32c52bA4b37f7d',
+      permanent: true,
+    },
+  ],
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     config.resolve.fallback = { fs: false, net: false, tls: false };
