94196bee627947d805fd2333944e8c0b31adb794
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-11-30 17:02:53 -0500

    fix: styles

--- app/puzzle/[slug]/write-up/page.tsx
+++ app/puzzle/[slug]/write-up/page.tsx
@@ -60,9 +60,9 @@ export default async function Page({ params }: { params: { slug: string } }) {
       id={`content-/puzzle/${normalizedSlug}/write-up`}
       role="tabpanel"
       aria-labelledby={`trigger-/puzzle/${normalizedSlug}/write-up`}
-      className="mx-auto flex max-w-[90rem] flex-col gap-12 px-4 pt-4 md:flex-row md:px-20 md:pt-8"
+      className="mx-auto flex max-w-[90rem] flex-col px-4 pt-4 lg:flex-row lg:gap-12 lg:px-20 lg:pt-8"
     >
-      <article className="prose max-w-full grow dark:prose-invert prose-h1:mb-4 prose-h1:text-3xl prose-h1:font-semibold prose-h1:tracking-tight prose-h1:text-gray-50 prose-h2:mb-2 prose-h2:mt-6 prose-h2:text-xl prose-h2:font-semibold prose-h2:tracking-tight prose-h2:text-gray-50 prose-h3:mb-2 prose-h3:mt-5 prose-h3:text-lg prose-h3:font-semibold prose-h3:tracking-tight prose-h3:text-gray-50 prose-strong:font-medium prose-strong:text-gray-50 prose-th:py-[1px] prose-img:my-2 prose-video:my-0 md:max-w-none prose-h1:md:text-4xl prose-h2:md:mb-4 prose-h2:md:mt-12 prose-h2:md:text-2xl prose-h3:md:mb-4 prose-h3:md:mt-6 prose-h3:md:text-xl prose-img:md:my-4">
+      <article className="prose max-w-none grow dark:prose-invert prose-h1:mb-4 prose-h1:text-3xl prose-h1:font-semibold prose-h1:tracking-tight prose-h1:text-gray-50 prose-h2:mb-2 prose-h2:mt-6 prose-h2:text-xl prose-h2:font-semibold prose-h2:tracking-tight prose-h2:text-gray-50 prose-h3:mb-2 prose-h3:mt-5 prose-h3:text-lg prose-h3:font-semibold prose-h3:tracking-tight prose-h3:text-gray-50 prose-strong:font-medium prose-strong:text-gray-50 prose-th:py-[1px] prose-img:my-2 prose-video:my-0 prose-h1:md:text-4xl prose-h2:md:mb-4 prose-h2:md:mt-8 prose-h2:md:text-2xl prose-h3:md:mb-4 prose-h3:md:mt-6 prose-h3:md:text-xl prose-img:md:my-4 lg:w-1/2 [&>*:nth-child(4)]:mt-0">
         <h1>Write-up for {puzzle.name ?? `Puzzle #${puzzle.id}`}</h1>
         <div className="not-prose flex items-center justify-between">
           <UserDisplay address={frontmatter.author} displaySocials={false}>
@@ -73,6 +73,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
             ) : null}
           </UserDisplay>
           <Button
+            className="hidden sm:flex"
             variant="outline"
             intent="neutral"
             rightIcon={<Github />}
@@ -81,11 +82,22 @@ export default async function Page({ params }: { params: { slug: string } }) {
           >
             Edit on GitHub
           </Button>
+          <Button
+            className="sm:hidden"
+            variant="outline"
+            intent="neutral"
+            rightIcon={<Github />}
+            href={`https://github.com/waterfall-mkt/curta-write-ups/blob/${writeUpUrlPath}`}
+            newTab
+          >
+            Edit
+          </Button>
         </div>
         <hr className="my-6 w-full rounded-full border-stroke" role="separator" />
         {content}
       </article>
-      <div className="flex w-full flex-col md:min-w-[22.5rem] md:max-w-[22.5rem]">
+      <hr className="my-6 w-full rounded-full border-stroke lg:hidden" role="separator" />
+      <div className="flex w-full flex-col lg:min-w-[22.5rem] lg:max-w-[22.5rem]">
         <div className="flex flex-col gap-2">
           <div className="text-sm text-gray-200">Contributors</div>
           <div className="flex flex-col gap-4 rounded-xl border border-stroke p-4">
