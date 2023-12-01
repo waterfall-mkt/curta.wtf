d7ec02f34116fa4b34ac06c90a48e9b27453d586
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-12-01 13:05:07 -0500

    feat: use `serialize` w/ `mdxOptions`

--- app/puzzle/[slug]/write-up/page.tsx
+++ app/puzzle/[slug]/write-up/page.tsx
@@ -1,13 +1,18 @@
 import { notFound } from 'next/navigation';
 import { cache } from 'react';
 
+import 'katex/dist/katex.min.css';
 import { Github } from 'lucide-react';
-import { useMDXComponents as getMDXComponents } from 'mdx-components';
-import { compileMDX } from 'next-mdx-remote/rsc';
+import { serialize } from 'next-mdx-remote/serialize';
+import rehypeKatex from 'rehype-katex';
+import rehypeMdxCodeProps from 'rehype-mdx-code-props';
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
 import type { Address } from 'viem';
 
 import { fetchPuzzleById, getChainIdAndId, getChainInfo } from '@/lib/utils';
 
+import CustomMDX from '@/components/templates/custom-mdx';
 import UserDisplay from '@/components/templates/user-display';
 import { Badge, Button } from '@/components/ui';
 
@@ -45,15 +50,21 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
   // Parse frontmatter and generate content from MDX.
   const source = await response.text();
-  const { content, frontmatter } = await compileMDX<{
-    author: Address;
-    contributors: Address[];
-    adapted_from?: string;
-  }>({
-    source,
-    components: getMDXComponents({}),
-    options: { parseFrontmatter: true },
+  const mdxSource = await serialize<
+    Record<string, unknown>,
+    {
+      author: Address;
+      contributors: Address[];
+      adapted_from?: string;
+    }
+  >(source, {
+    parseFrontmatter: true,
+    mdxOptions: {
+      remarkPlugins: [remarkGfm, remarkMath],
+      rehypePlugins: [rehypeKatex, rehypeMdxCodeProps],
+    },
   });
+  const frontmatter = mdxSource.frontmatter;
 
   return (
     <div
@@ -94,7 +105,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
           </Button>
         </div>
         <hr className="my-6 w-full rounded-full border-stroke" role="separator" />
-        {content}
+        <CustomMDX {...mdxSource} />
       </article>
       <hr className="my-6 w-full rounded-full border-stroke lg:hidden" role="separator" />
       <div className="flex h-fit w-full flex-col lg:sticky lg:top-[9.25rem] lg:min-w-[22.5rem] lg:max-w-[22.5rem]">
