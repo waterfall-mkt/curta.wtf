300dfe96c48b8290f03d05e93023a34e8b7c9c99
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-12-01 22:56:14 -0500

    feat: add H3

--- components/templates/mdx/index.tsx
+++ components/templates/mdx/index.tsx
@@ -2,5 +2,6 @@ import Callout from './callout';
 import Description from './description';
 import H2 from './h2';
 import H3 from './h3';
+import H4 from './h4';
 
-export { Callout, Description, H2, H3 };
+export { Callout, Description, H2, H3, H4 };
