982eb6ce55a50e94bfaf371ba3fa4273dd5ee85e
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-12-07 12:55:12 -0500

    feat: add problem display

--- app/course/[slug]/page.tsx
+++ app/course/[slug]/page.tsx
@@ -1,3 +1,48 @@
-export default function Page() {
-  return 'TODO';
+import { notFound } from 'next/navigation';
+
+import CourseProblemDisplay from './(components)/problem-display';
+
+import { fetchCourseById, getChainIdAndId } from '@/lib/utils';
+
+// -----------------------------------------------------------------------------
+// Page
+// -----------------------------------------------------------------------------
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const ids = getChainIdAndId(params.slug);
+
+  // Return 404 if `slug` is an invalid format.
+  if (!ids) return notFound();
+
+  const { chainId, id } = ids;
+
+  const { data: course, error } = await fetchCourseById(id, chainId);
+
+  // Return 404 if `course` is `null` or there was an `error` in fetching the
+  // data.
+  if (!course || error) return notFound();
+
+  const languages = ['Bytecode']
+    .concat(course.solidity ? ['Solidity'] : [])
+    .concat(course.huff ? ['Huff'] : []);
+
+  const normalizedSlug = decodeURIComponent(params.slug.toLowerCase());
+
+  return (
+    <div
+      id={`content-/course/${normalizedSlug}`}
+      className="mx-auto mt-4 flex max-w-[90rem] flex-col gap-4 px-4 md:flex-row md:gap-6 lg:px-20"
+      role="tabpanel"
+      aria-labelledby={`trigger-/course/${normalizedSlug}`}
+    >
+      <CourseProblemDisplay course={course} languages={languages} />
+      {/* Solve form */}
+    </div>
+  );
 }
+
+// -----------------------------------------------------------------------------
+// Next.js config
+// -----------------------------------------------------------------------------
+
+export const revalidate = 300;
